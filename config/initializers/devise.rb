bb64b77acf163cfb32fe92476c4a4785743c1bb7
tomiyasu <tomykyou.0211@gmail.com>
2021-09-09 22:00:14 +0900

    usernameを登録可能にする

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  require 'devise/orm/active_record'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
@@ -158,7 +158,7 @@ Devise.setup do |config|
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
   config.reconfirmable = true
-
+  config.scoped_views = true
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
 
