32dd0dc5f8cf5124c5048ac5fc7d9c6fe2f2e8c6
tomiyasu <tomykyou.0211@gmail.com>
2021-09-09 23:22:00 +0900

    show,edit,new,formビュー修正とルート編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  get 'mainnote/top'
-  get 'mainnote/show'
-  get 'mainnote/new'
-  get 'mainnote/edit'
-  get 'mainnote/_form'
-  get 'mainnote/mypage'
-  get 'mainnote/about'
-  get 'mainnote/userpg'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
   get 'sessions/new'
-  root "mainnotes#top"
-  get 'mainnnotes/mypage', to: 'mainnotes#mypage'
-  get 'mainnnotes/about', to: 'mainnotes#about'
-  get 'mainnnotes/usershow', to: 'mainnotes#usershow'
-  get 'mainnnotes/userpg/:id', to: 'mainnotes#userpg'
+  root "mainnote#top"
+  get 'mainnnote/mypage', to: 'mainnote#mypage'
+  get 'mainnnote/about', to: 'mainnote#about'
+  get 'mainnnote/usershow', to: 'mainnote#usershow'
+  get 'mainnnote/userpg/:id', to: 'mainnote#userpg'
 
   resources :mainnote do
     resources :notecomments
