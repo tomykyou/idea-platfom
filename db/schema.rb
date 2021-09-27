a106e62d3b720bd3edd735089112f1aa2f07b66f
tomiyasu <tomykyou.0211@gmail.com>
2021-09-27 21:05:44 +0900

    gem アドミンの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_033047) do
+ActiveRecord::Schema.define(version: 2021_09_27_115541) do
 
   create_table "mainnotes", force: :cascade do |t|
     t.string "text"
