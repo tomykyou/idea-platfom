6baf8ce29c784c62c4c7a8602dc196a44af084a4
tomiyasu <tomykyou.0211@gmail.com>
2021-10-17 12:43:21 +0900

    ログアウト機能アップデート

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
