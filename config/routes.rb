3e5667b0ffd780a4755e10606094d748e4f2eadf
tomiyasu <tomykyou.0211@gmail.com>
2021-10-17 12:29:36 +0900

    メニューバー適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- 
+
   get 'sessions/new'
   root "mainnote#top"
   get 'mainnnote/mypage', to: 'mainnote#mypage'
