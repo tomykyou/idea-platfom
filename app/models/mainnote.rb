0ba62281a71931a726d9f07a3000c268d5f34eda
tomiyasu <tomykyou.0211@gmail.com>
2021-11-04 00:58:55 +0900

    トップページのデザイン修正

--- app/models/mainnote.rb
+++ app/models/mainnote.rb
@@ -2,8 +2,8 @@ class Mainnote < ApplicationRecord
   mount_uploader :image1, ImageUploader
   mount_uploader :image2, ImageUploader
   mount_uploader :image3, ImageUploader
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true,length: {minimum:10, maximum:25}
+  validates :text, presence: true,length: {minimum:30}
   belongs_to :user
   has_many :notecomments, dependent: :destroy
 end
