a423e917caa04bbaf4eb20921bfb4c8768e9d188
tomiyasu <tomykyou.0211@gmail.com>
2021-11-19 01:13:34 +0900

    ログイン後のレイアウト調整

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,6 +65,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)  
   end
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end 
 
   def after_update_path_for(_resource)
     mainnnote_mypage_path 
