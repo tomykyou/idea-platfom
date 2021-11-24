3095e0cc982799f6f64bc1930acf3b5b80d808c0
tomiyasu <tomykyou.0211@gmail.com>
2021-11-25 01:58:59 +0900

    工夫投稿でエラー１つのみ表示する

--- app/models/mainnote.rb
+++ app/models/mainnote.rb
@@ -2,8 +2,25 @@ class Mainnote < ApplicationRecord
   mount_uploader :image1, ImageUploader
   mount_uploader :image2, ImageUploader
   mount_uploader :image3, ImageUploader
-  validates :title, presence: true,length: {minimum:10, maximum:25}
-  validates :text, presence: true,length: {minimum:30}
+  validate :title_valid
+  def title_valid
+    if title.blank?
+      errors.add(:title, :blank)
+    elsif title.length < 10
+      errors.add(:title, 'は10文字以上で入力してください')
+    elsif title.length > 25
+      errors.add(:title, 'は25文字以下で入力してください')
+    end
+  end
+  validate :text_valid
+  def text_valid
+    if text.blank?
+      errors.add(:text, :blank)
+    elsif text.length > 500
+      errors.add(:text, 'は500文字以内で入力してください')
+    end  
+  end
+
   belongs_to :user
   has_many :notecomments, dependent: :destroy
 end
