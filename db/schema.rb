ff715122cab7438c36aa9f1bf8af0fc72d74fe48
tomiyasu <tomykyou.0211@gmail.com>
2021-12-05 23:34:10 +0900

    deviseに職業の情報を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_034849) do
+ActiveRecord::Schema.define(version: 2021_12_02_170341) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "mainnote_id", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_034849) do
     t.string "username"
     t.string "image"
     t.string "award"
+    t.string "occupation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
